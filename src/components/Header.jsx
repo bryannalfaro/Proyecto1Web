4c3aec94afc37846674f203e577e43a614075ad5
Bryann Alfaro <alf19372@uvg.edu.gt>
2021-04-02 19:35:44 -0600

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,15 @@ import '../styles/header.css'
 
 const Header = () => (
   <div className="header">
-    <button aria-label="Save" type="button" className="login">Iniciar Sesion</button>
+    <button aria-label="Save" type="button" className="login">INICIAR SESIÓN</button>
     <img src={logo} alt="logo" />
-    <button aria-label="Save" type="button" className="whatss" />
-    <button aria-label="Save" type="button" className="market">
-      <span className="icon" />
-      MI CANASTA
-    </button>
+    <div className="group">
+      <button aria-label="Save" type="button" className="whatss" />
+      <button aria-label="Save" type="button" className="market">
+        <span className="icon" />
+        MI CANASTA
+      </button>
+    </div>
   </div>
 )
 
