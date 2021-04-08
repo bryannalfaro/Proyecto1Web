50755a22f6e6dddd06eaf9a16cb3b3ae5a20d906
Bryann Alfaro <alf19372@uvg.edu.gt>
2021-04-08 13:04:20 -0600

    duo style

--- src/components/Duo.jsx
+++ src/components/Duo.jsx
@@ -5,11 +5,11 @@ const Duo = () => (
   <div className="padre2">
     <div className="izq">
       <div className="image1" />
-      <div className="block" />
+      <div className="block">&#160;</div>
     </div>
     <div className="der">
       <div className="image2" />
-      <div className="block2" />
+      <div className="block2">&#160;</div>
     </div>
   </div>
 )
