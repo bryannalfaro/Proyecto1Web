78ecfc6a00cccc30f5120ca7b9fe160aae9a3291
Bryann Alfaro <alf19372@uvg.edu.gt>
2021-04-09 10:19:23 -0600

    header and email

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/forms.css'
+import icon from '../images/email.png'
 
 const Form = () => (
   <div className="form">
@@ -11,7 +12,9 @@ const Form = () => (
       <div className="padreicon">
         <div className="inp">
           <input placeholder="Email" className="email" />
-          <div className="emailicon" />
+          <div className="emailicon">
+            <img src={icon} alt="icon" className="iconem" />
+          </div>
         </div>
       </div>
     </div>
